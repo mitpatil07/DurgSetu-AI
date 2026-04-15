db6c6d6ee462c19e642607580df5063524a55d80
Mitesh Patil <mitesh8767@gmail.com>
2026-04-15 10:43:23 +0530

    fix : smtp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,6 +25,23 @@ USE_X_FORWARDED_HOST = True
 
 
 # -----------------------------
+# EMAIL (FIXED)
+# -----------------------------
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
+EMAIL_TIMEOUT = 30
+
+# -----------------------------
 # APPS
 # -----------------------------
 INSTALLED_APPS = [
